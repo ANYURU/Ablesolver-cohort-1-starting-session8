ee8e48ff8b33b03091e7bd94c94a4e9810b05634
anyuru <davidderrickanyuru@gmail.com>
2021-07-28 11:50:51 +0300

    Validating character input completed.

--- session8/src/validator.js
+++ session8/src/validator.js
@@ -1,15 +1,21 @@
-const CHECK_EMPTY = (event) => {
-    
-    //console.log(event)
-    if (event.target.value == '' || event.target.value == null) {
-        event.target.classList.add("is-invalid")
+const CHECK_EMPTY = (event, length =8) => {
+    const  ELEMENT = event.target
+    console.log(event)
+    if (ELEMENT.value == '' || event.target.value == null) {
+        ELEMENT.classList.add("is-invalid")
 
         // alert('Field required')
         // console.log(event.target)
         return false
     }
 
-    event.target.classList.remove("is-invalid")
+    if (ELEMENT.value.length < length) {
+        ELEMENT.classList.add("is-invalid")
+        return false
+    }
+
+
+    ELEMENT.classList.remove("is-invalid")
     return true
-    
+
 } 
