29936cfd6d84741cd03ffa5d4ee43f920c810c0f
anyuru <davidderrickanyuru@gmail.com>
2021-08-23 12:46:26 +0300

    Removed use arithmetic

--- session10/timers.js
+++ session10/timers.js
@@ -1,9 +1,13 @@
-setTimeout(() => {
-    //runs after 2 seconds
-    console.log('Ran after 2 seconds')
-}, 2000);
+// setTimeout(() => {
+//     //runs after 2 seconds
+//     console.log('Ran after 2 seconds')
+// }, 2000);
 
-setTimeout(() => {
-    // runs after 50 milliseconds
-    console.log('Ran after 50 milliseconds');
-}, 50)
+// setTimeout(() => {
+//     // runs after 50 milliseconds
+//     console.log('Ran after 50 milliseconds');
+// }, 50)
+
+//setTimeout with parameters
+
+let greeting = (name, msg) => console.log(`${msg} ${name}`)
