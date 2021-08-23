0c4dc7cf5fec0625f336af2c07856f13be053a94
anyuru <davidderrickanyuru@gmail.com>
2021-08-23 12:50:37 +0300

    setTimeout with parameters.

--- session10/timers.js
+++ session10/timers.js
@@ -11,3 +11,6 @@
 //setTimeout with parameters
 
 let greeting = (name, msg) => console.log(`${msg} ${name}`)
+ 
+setTimeout(greeting, 1000, 'David', 'Hola')
+setTimeout(greeting, 500, 'Abudi', 'Agandi')
