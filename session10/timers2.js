38115d9d974fcf5b98b47ab9a74049417585f43e
anyuru <davidderrickanyuru@gmail.com>
2021-08-24 11:45:40 +0300

    Consoling the time at specific time intervals.

--- session10/timers2.js
+++ session10/timers2.js
@@ -6,11 +6,18 @@
 // console.log('before')
 
 
-setTimeout(() => {
-    console.log(`The first`) 
-}, 0)
+// setTimeout(() => {
+//     console.log(`The first`) 
+// }, 0)
 
 
-setTimeout(() => {
-    console.log(`The Second`) 
-}, 0)
+// setTimeout(() => {
+//     console.log(`The Second`) 
+// }, 0)
+
+// setInterval: Running a callback forever on specific intervals
+
+setInterval(()=>{
+    let date = new Date();
+    console.log(date.toLocaleTimeString())
+}, 1000)
