d2a0462689f9f1e412701e5940b001a274b223d8
anyuru <davidderrickanyuru@gmail.com>
2021-09-08 10:38:02 +0300

    Posting to the backend and logging using axios library.

--- session10/request-handling-2.js
+++ session10/request-handling-2.js
@@ -1,3 +1,4 @@
+/*
 const https = require('https')
 
 const data = new TextEncoder().encode(
@@ -30,3 +31,17 @@ request.on('error', error => console.error(error))
 
 request.write(data)
 request.end()
+*/
+// Using a third party Library (axios) to perform HTTP requests
+
+const axios = require('axios')
+
+axios 
+    .post('https://jsonplaceholder.typicode.com/todos', {
+        todo: 'Buy the milk'
+    })
+    .then(response => {
+        console.log(`statusCode: ${response.status}`)
+        console.log(response)
+    })
+    .catch(error => console.error(error))
