f6e26d586b0808b70e8f582c06ad1896f7b24dcc
anyuru <davidderrickanyuru@gmail.com>
2021-09-13 10:24:05 +0300

    Normalizing a path.

--- session10/file-path-2.js
+++ session10/file-path-2.js
@@ -15,3 +15,11 @@ console.log(absPath)
 
 const absPath2 = path.resolve('tmp', 'file.txt')
 console.log(absPath2)
+
+// If you specify a forward slash / on the first argument the resolve will assume that the file is an absolute path
+const absPath3 = path.resolve('/etc', 'joe.txt')
+console.log(absPath3)
+
+//You also use ../ and double slashes
+const absPath4 = path.normalize('/Users/joe/..//test.txt')
+console.log(absPath4)
