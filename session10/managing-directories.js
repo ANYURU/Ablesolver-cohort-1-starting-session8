df9c820e567aaf20eaf13f80b0239a4941565987
anyuru <davidderrickanyuru@gmail.com>
2021-09-14 10:41:41 +0300

    Aceessing a non operating system directory.

--- session10/managing-directories.js
+++ session10/managing-directories.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+/*
 //Access of the operating system's /etc directory
 const dirname = '/etc'
 fs.access(dirname, (err) => {
@@ -9,4 +10,14 @@ fs.access(dirname, (err) => {
     }
     console.log(`It's possible to access the directory ${dirname}`) 
 })
+*/
 
+// Relative directory
+const dirname2 = 'images'
+fs.access(dirname2, (err) => {
+    if(err) {
+        console.log(err)
+        return 
+    }
+    console.log(`It's possible to access the directory ${dirname2}`) 
+}) 
