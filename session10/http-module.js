6594b1601f2f8397ee0a1c5391352461d2571ac3
anyuru <davidderrickanyuru@gmail.com>
2021-09-20 10:49:01 +0300

    Working with the http module.

---
+++ session10/http-module.js
@@ -0,0 +1,7 @@
+const http = require('http')
+// Get information about the http module
+console.log(http)
+
+console.log(http.METHODS) //Output all the Methods
+
+console.log(http.STATUS_CODES) //Output all the http STATUS_CODES and descriptions
